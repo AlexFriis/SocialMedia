d66a2e9a584066c52df0f4e435ab014c09b8cefe
Mathias <math8802@stud.kea.dk>
2021-03-07 15:06:41 +0100

    aa

--- src/main/java/kea/socialmedia/controllers/PostController.java
+++ src/main/java/kea/socialmedia/controllers/PostController.java
@@ -1,6 +1,49 @@
 package kea.socialmedia.controllers;
 
+
+import kea.socialmedia.models.Post;
+import kea.socialmedia.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/gameMedia/")
+
 public class PostController {
 
+    private final PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("dashboard")
+    public String showDashboard(Model model) {
+        model.addAttribute("posts", postService.getAllPost());
+        return "dashboard";
+    }
+
+    @GetMapping("submit")
+    public String showSubmitPostForm(Post post) {
+        return "submit-post";
+    }
+
+    @PostMapping("submit-post")
+    public String addStudent(@Validated Post post, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "submit-post";
+        }
+        model.addAttribute("successMessage", "Post has been registered successfully!");
+        model.addAttribute("post", post);
+        postService.addPost(post);
+        return "dashboard";
+    }
 
 }
--- src/main/java/kea/socialmedia/service/PostService.java
+++ src/main/java/kea/socialmedia/service/PostService.java
@@ -1,10 +1,12 @@
 package kea.socialmedia.service;
 
 import kea.socialmedia.models.Post;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class PostService {
 
     List<Post> postList = new ArrayList<>();
@@ -16,4 +18,5 @@ public class PostService {
     public void addPost(Post post) {
         postList.add(post);
     }
+
 }
